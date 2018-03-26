f9e7803e3a36523febd5f6456decb0ae2a39ea91
Jimbas <jimbas@MekbukPro.local>
2018-03-26 23:03:46 +0700

    initial commit

--- v1/ajaxauth.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-	include_once('app.php');
-	include_once('mncdigweb.php');
-
-	$response = mncdigAuth(MNC_DIGITAL_CMURL, MERCHANT_STATIC_KEY, MERCHANT_REGISTERED_URL);
-	print(json_encode($response));
--- v1/clipsmetube/mncdigweb.php
+++
@@ -1,1 +0,0 @@
-../mncdigweb.php
